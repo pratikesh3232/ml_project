f3b0451db1ce766cda01444d0c5542bc5efcc155
pratikesh3232 <117288190+pratikesh3232@users.noreply.github.com>
2023-10-23 12:55:18 +0530

    3.0

--- src/utils.py
+++ src/utils.py
@@ -18,6 +18,15 @@ def save_obj(file_path,obj):
         raise CustmeException(e,sys)
     
 
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_objt:
+            return pickle.load(file_objt)
+    except Exception as e:
+        raise CustmeException(e, sys)
+    
+    
+
 def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report={}
